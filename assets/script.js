cda1a934eb6ce329b4b4b0b2d0f81508df1b1568
justin anderson <jcanderson@gmail.com>
2023-01-25 18:50:29 -0500

    questions are working

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,28 @@ let questions = [
         opt4 : "wrong",
         correct : "opt1",
     },{
-        question : "here is the 1st question:",
+        question : "here is the 2nd question:",
+        opt1 : "wrong",
+        opt2 : "wrong",
+        opt3 : "right",
+        opt4 : "wrong",
+        correct : "opt3",
+    },{
+        question : "here is the 3rd question:",
+        opt1 : "right",
+        opt2 : "wrong",
+        opt3 : "wrong",
+        opt4 : "wrong",
+        correct : "opt1",
+    },{
+        question : "here is the 4th question:",
+        opt1 : "wrong",
+        opt2 : "wrong",
+        opt3 : "right",
+        opt4 : "wrong",
+        correct : "opt3",
+    },{
+        question : "here is the 5th question:",
         opt1 : "wrong",
         opt2 : "wrong",
         opt3 : "right",
@@ -27,12 +48,14 @@ let questions = [
     }
 ]
 
+const lastQuestion = questions.length - 1;
+
 let runningQuestion = 0;
 
 function renderQuestion() {
     let q = questions[runningQuestion];
 
-    question.innerHTML = "<p>" +q.question +"<p>";
+    question.innerHTML = "<p>" +q.question +"</p>";
     opt1.innerHTML = q.opt1;
     opt2.innerHTML = q.opt2;
     opt3.innerHTML = q.opt3;
@@ -46,4 +69,33 @@ function startGame() {
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
     renderQuestion();
+}
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+
+    if (runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion()
+    }else{
+        endGame();
+    }
+}
+
+function correctAnswer(){
+    console.log("correct")
+}
+
+function wrongAnswer(){
+    console.log("wrong")
+}
+
+function endGame() {
+    console.log("game over")
+    startButton.classList.remove('hide')
+    questionContainerElement.classList.add('hide')
 }
